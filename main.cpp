db4a0f79e2a75aa36da1bb4adf587946bd2e59a1
Muhammad Taha Raees <taha.raees13@gmail.com>
2021-05-15 21:56:01 +0500

    added classes and its memebers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
-int main()
+#include<iostream>
+using namespace std;
+class Location{
+	string address;
+	string city;
+	Hospital h1;//has a relation
+};
+class Doctor{
+	string name;
+	string designation; //type of doctor e.g psychiatrist, cardiologist, general physician, dermatologist
+    string exp;
+	int contact;
+};
+class Hospital{
+	string name;
+	string address;
+	string timing;
+    //has a relation
+	Doctor d1;
+	Lab l1;
+};
+class Lab
 {
-    return 0;
-    //check
-}
+	string timing;
+	string facilities;
+	
+};
+//Child classes of hospital
+class GeneralHospital:public Hospital
+{
+    string deaprtments;
+};
+class SpecializedHospital:public Hospital
+{
+	string type;
+};
