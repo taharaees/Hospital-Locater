a4cec7d50f2fe446d0825cd45ccfa222ddf46082
Muhammad Taha Raees <taha.raees13@gmail.com>
2021-05-16 17:45:57 +0500

    correct code

--- main.cpp
+++ main.cpp
@@ -1,110 +1,119 @@
 #include<iostream>
 using namespace std;
-class Location{
-	string address;
-	string city;
-	Hospital h1;//has a relation
-
-	public:
-		Location()
-		{
-			address="xxxx";
-			city="xxxx";
-		}
-		Location(string address,string city, string H)
-		{
-			this->address=address;
-			this->city=city;
-		}
-		
-};
-class Doctor{
+class Doctor
+{
 	string name;
 	string designation; //type of doctor e.g psychiatrist, cardiologist, general physician, dermatologist
     string exp;
 	int contact;
 	public:
-		Doctor()
-		{
-			name="xxxx";
-			designation="xxxx"; 
-    		exp="xxxx";
-			contact=0;
-			
-		}
-		Doctor(string name,string designation,string exp,int contact)
-		{
-			this->name=name;
-			this->designation=designation;
-			this->exp=exp;
-			this->contact=contact;
-		}
-};
-class Hospital{
-	public:
-	string name;
-	string address;
-	string timing;
-    //has a relation
-	Doctor d1;
-	Lab l1;
+	//Constructors
+	Doctor()
+	{
+		name="xxxx";
+		designation="xxxx"; 
+    	exp="xxxx";
+		contact=0;		
+	}
+	Doctor(string name,string designation,string exp,int contact)
+	{
+		this->name=name;
+		this->designation=designation;
+		this->exp=exp;
+		this->contact=contact;
+	}
 };
 class Lab
 {
 	string timing;
 	string facilities;
 	public:
-		Lab()
-		{
-			timing="xxxx";
-			facilities="xxxx";
-		}
-		Lab(string timing,string facilities)
-		{
-			this->timing=timing;
-			this->facilities;
-		}
+	//Constructors
+	Lab()
+	{
+		timing="xxxx";
+		facilities="xxxx";
+	}
+	Lab(string timing,string facilities)
+	{
+		this->timing=timing;
+		this->facilities=facilities;
+	}
+};
+class Hospital
+{
+	string name;
+	string address;
+	string timing;
+	public:
+	Hospital()
+	{
+		name="xxxx";
+		address="xxxx";
+		timing="xxxx";
+	}
+	Hospital(string name, string address, string timing)
+	{
+		this->name=name;
+		this->address=address;
+		this->timing=timing;
+	}
+	
+    //has a relation
+	Doctor d1;
+	Lab l1;
+};
+class Location
+{
+	string address;
+	string city;
+	//has a realtion
+	Hospital h1;
+
+	public:
+	//Constructors
+	Location()
+	{
+		address="xxxx";
+		city="xxxx";
+	}
+	Location(string address,string city)
+	{
+		this->address=address;
+		this->city=city;
+	}
+		
 };
 //Child classes of hospital
 class GeneralHospital:public Hospital
 {
     string departments;
     public:
-    GeneralHospital(string departments,string name,string address,string timing)
-    {
-    	this->departments=departments;	
-    	this->name=name;
-    	this->address=address;
-    	this->timing=timing;
-	}
-	GeneralHospital()
+	//Constructors
+	GeneralHospital():Hospital()
 	{
 		departments="xxxx";
-		name="xxxx";
-		address="xxxx";
-		timing="xxxx";
+	}
+    GeneralHospital(string departments,string name,string address,string timing): Hospital(name, address, timing)
+    {
+    	this->departments=departments;
 	}
 };
 class SpecializedHospital:public Hospital
 {
 	string type;
 	public:
-		SpecializedHospital(string type,string name,string address,string timing)
-		{
-			this->type=type;
-			this->name=name;
-    		this->address=address;
-    		this->timing=timing;
-		}
-		SpeciaizedHospital()
+	//Constructors
+	SpecializedHospital(): Hospital()
 		{
 			type="xxxx";
-			name="xxxx";
-			address="xxxx";
-			timing="xxxx";
+		}
+	SpecializedHospital(string type,string name,string address,string timing): Hospital(name, address, timing)
+		{
+			this->type=type;
 		}
 };
 int main()
 {
-	
+	return 0;
 }
