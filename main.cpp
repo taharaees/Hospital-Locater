22954f7b219947a636ed344e43f119b7e5d129e1
taharaees <40723693+taharaees@users.noreply.github.com>
2021-05-16 18:08:16 +0500

    setter getter of docotr class added

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ class Doctor
 	{
 		name="xxxx";
 		designation="xxxx"; 
-    	exp="xxxx";
+    	        exp="xxxx";
 		contact=0;		
 	}
 	Doctor(string name,string designation,string exp,int contact)
@@ -21,6 +21,31 @@ class Doctor
 		this->designation=designation;
 		this->exp=exp;
 		this->contact=contact;
+	}
+	//setter functions
+	void setName(string n)
+	{
+		name=n;
+	}
+	void setDesig(string d)
+	{
+		designation=d;
+	}
+	void setExp(string e)
+	{
+		exp=e;
+	}
+	void setContact(int c)
+	{
+		contact=c;
+	}
+	// getter functions
+	void display()
+	{
+		cout<<"Name of doctor: "<<name<<endl;
+		cout<<"Designation: "<<designation<<endl;
+		cout<<"Experience: "<<exp<<endl;
+		cout<<"Contact: "<<contact<<endl;
 	}
 };
 class Lab
