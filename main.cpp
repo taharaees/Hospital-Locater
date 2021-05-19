7c224582c87c883d2a5f25674fa4e3646870b6ae
kamran <75690317+mkacs@users.noreply.github.com>
2021-05-19 23:48:35 +0500

    Update main.cpp
    Final code updated

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ class Doctor
 		cout<<"Name of doctor: "<<name<<endl;
 		cout<<"Designation: "<<designation<<endl;
 		cout<<"Experience: "<<exp<<endl;
-		cout<<"Contact: "<<contact<<endl;
+		cout<<"Contact: 0"<<contact<<endl;
 	}
 };
 class Lab
@@ -127,10 +127,10 @@ class Hospital
 
 	
 	//display function
-	void display()
+	void Hdisplay()
 	{
 		cout << "Hospital Name: " << name<<endl;
-		cout << "Address: " << address<<endl;
+		cout << "   Address: " << address<<endl;
 	}
 	void labDisp()
 	{
@@ -141,47 +141,7 @@ class Hospital
 		d1.display();
 	}
 };
-class Location
-{
-	string area;
-	//has a realtion
-	Hospital h1;
 
-	public:
-	//Constructors
-	Location()
-	{
-		area="xxxx";
-		h1.setName("xxxx");
-		h1.setAddress("xxxx");
-	}
-	Location(string area, string name, string address, string dn,string des, string ex, int con,string fa,string ti)
-	{
-		this->area=area;
-		Hospital h2(name,address,dn,des,ex,con,fa,ti);
-		h1=h2;
-	}
-	//setter
-	void setAddress(string address)
-	{
-		this->area=area;
-	}
-
-	//getter
-	void display()
-	{
-		h1.display();
-	}
-	void labDisp()
-	{
-		h1.labDisp();
-	}
-	void DocDisp()
-	{
-		h1.DocDisp();
-	}
-		
-};
 //Child classes of hospital
 class SpecializedHospital:public Hospital
 {
@@ -192,7 +152,7 @@ class SpecializedHospital:public Hospital
 		{
 			type="xxxx";
 		}
-	SpecializedHospital(string type,string name,string address,string timing, string dn,string des, string ex, int con,string fa,string ti): Hospital(name, address,dn,des,ex,con,fa,ti)
+	SpecializedHospital(string type,string name,string address, string dn,string des, string ex, int con,string fa,string ti): Hospital(name, address,dn,des,ex,con,fa,ti)
 		{
 			this->type=type;
 		}
@@ -204,9 +164,55 @@ class SpecializedHospital:public Hospital
 		//getter
 		void display()
 		{
-			cout<<"Type is: "<<type<<endl;
+			Hdisplay();
+			cout<<"   Type of hospital is: "<<type<<endl;
 		}
 };
+
+
+
+class Location
+{
+	string area;
+	//has a realtion
+	SpecializedHospital sh1;
+
+	public:
+	//Constructors
+	Location()
+	{
+		area="xxxx";
+		sh1.setName("xxxx");
+		sh1.setAddress("xxxx");
+	}
+	Location(string area, string type, string name, string address, string dn,string des, string ex, int con,string fa,string ti)
+	{
+		this->area=area;
+		SpecializedHospital sh2(type, name,address,dn,des,ex,con,fa,ti);
+		sh1=sh2;
+	}
+	//setter
+	void setAddress(string address)
+	{
+		this->area=area;
+	}
+
+	//getter
+	void display()
+	{
+		sh1.display();
+	}
+	void labDisp()
+	{
+		sh1.labDisp();
+	}
+	void DocDisp()
+	{
+		sh1.DocDisp();
+	}
+		
+};
+
 
 int main()
 {
@@ -215,21 +221,23 @@ int main()
 	cout<<"Do you want to search for Hospital or Lab?"<<endl;
 	cin>>search;
 	//HOSPITALS
-	if(search=="Hospital")
+	if(search=="Hospital"||search=="hospital")
 	{
 		cout<<"Enter Location: "<<endl;
 	    cin>>loc;
-		if(loc=="DHA")
+		if(loc=="DHA"||loc=="dha")
 	    {
-		    Location dha1("DHA", "NMC","adadadadad","taha","psych","bohat",111,"mri","8AM-6PM");
-			Location dha1A("DHA", "NMC","adadadadad","Malaecah","dermo","2yrs",222,"mri","8AM-6PM");
-			Location dha1B("DHA", "NMC","adadadadad","Alina","neuro","3yrs",333,"mri","8AM-6PM");
-		    Location dha2("DHA", "Bayview","bay","Kamran","phys","boh",444,"xray","8AM-8PM");
-			Location dha2A("DHA", "NMC","adadadadad","MTR","child","8yrs",555,"mri","8AM-6PM");
-			Location dha2B("DHA", "NMC","adadadadad","Altaf","psych","6yrs",666,"mri","8AM-6PM");
-		    Location dha3("DHA", "NMC","adadadadad","Aisha","Gyna","5yrs",777,"blood test","8AM-7PM");
-			Location dha3A("DHA", "NMC","adadadadad","Ali","child","4yrs",888,"mri","8AM-6PM");
-			Location dha3B("DHA", "NMC","adadadadad","Malaika","phys","2yrs",999,"mri","8AM-6PM");
+		    Location dha1("DHA","Skin care", "NMC","sunset boulevard street 11","Taha","psychiatrist","more than 8 years of experience",211345445,"mri","8AM-6PM");
+			Location dha1A("DHA","Skin care", "NMC","sunset boulevard street 11","Malaecah","Dermotologist","2 years of experience",213476672,"mri","8AM-6PM");
+			Location dha1B("DHA","Skin care", "NMC","sunset boulevard street 11","Alina","Neurologist","3 years of experience",217468928,"mri","8AM-6PM");
+		    Location dha2("DHA", "cardic and heart disease", "Bayview", "6th lane, phase 8","Kamran","Cardiologist","7 years of experience",333022289,"xray","8AM-8PM");
+			Location dha2A("DHA", "cardic and heart disease", "Bayview", "6th lane, phase 8","Shoaib","Pediatrician","at house job",213578734,"mri","8AM-8PM");
+			Location dha2B("DHA", "cardic and heart disease", "Bayview", "6th lane, phase 8","Altaf","psych","6 years of experience",21376657,"mri","8AM-6PM");
+		    Location dha3("DHA", "Abdomen and pelvis", "Shifa Hospital","sailor st, bukhari lane 6","Aisha","Gynecologist","12 years of experience",21345883,"blood test","8AM-7PM");
+			Location dha3A("DHA", "Abdomen and pelvis", "Shifa Hospital","sailor st, bukhari lane 6","Ali","Rheumatologist","4 years of experience",21900786,"mri","8AM-6PM");
+			Location dha3B("DHA", "Abdomen and pelvis", "Shifa Hospital","sailor st, bukhari lane 6","Malaika","Genral physician","3 years of experience",21674699,"full body scan","8AM-6PM");
+			
+			
 		    cout<<"1: "; 
 		    dha1.display();
 		    cout<<endl;
@@ -271,17 +279,17 @@ int main()
 
 			}
 		}
-		else if(loc=="Nazimabad")
+		else if(loc=="Nazimabad"||loc=="nazimabad")
 		{
-			Location naz1("Nazimabad", "Lifeline", "khjhkgjgkjh","Hina","psyc","5yrs",111,"medical","9AM-5PM");
-			Location naz1A("Nazimabad", "Lifeline", "khjhkgjgkjh","Alia","psyc","7yrs",222,"medical","9AM-5PM");
-			Location naz1B("Nazimabad", "Lifeline", "khjhkgjgkjh","Imran","psyc","3yrs",333,"medical","9AM-5PM");
-			Location naz2("Nazimabad", "Ziauddin Hospital","adadadadad","Ahmed","dentist","normal",444,"mri","8AM-6PM");
-			Location naz2A("Nazimabad", "Lifeline", "khjhkgjgkjh","Blah","child","2yrs",555,"medical","9AM-5PM");
-			Location naz2B("Nazimabad", "Lifeline", "khjhkgjgkjh","tah","dermo","5yrs",666,"medical","9AM-5PM");
-		    Location naz3("Nazimabad", "Liaquat National","liaquat road","Kamran","cardiologist","jis school main tum prhte thy uskay principal",333022289,"xray","8AM-8PM");
-			Location naz3A("Nazimabad", "Lifeline", "khjhkgjgkjh","Gumnaam","psyc","2yrs",00002,"medical","9AM-5PM");
-			Location naz3B("Nazimabad", "Lifeline", "khjhkgjgkjh","Gumnaam2","physio","5yrs",00002,"medical","9AM-5PM");
+			Location naz1("Nazimabad", "ENT","Lifeline", "st near Aisha masjid","Hina","Ophthalmologist","5 years of experience",2137619,"medical","9AM-5PM");
+			Location naz1A("Nazimabad","ENT", "Lifeline", "16th st, near Aisha masjid","Alia","Dentist","7 years of experience",343768793,"medical","9AM-5PM");
+			Location naz1B("Nazimabad","ENT", "Lifeline", "16th st, near Aisha masjid","Imran","ENT specialist","3 years of experience",2138789,"medical","9AM-5PM");
+			Location naz2("Nazimabad", "skin","Ziauddin Hospital","Nazimabad 3, block 3","Ahmed","Eye specialist","9 years of experience",21084980,"mri","8AM-6PM");
+			Location naz2A("Nazimabad","skin", "Ziauddin Hospital", "Nazimabad 3, block 3","Asjid","Endocrinologist","2 years of experience",21083979,"medical","9AM-5PM");
+			Location naz2B("Nazimabad","skin", "Ziauddin Hospital", "Nazimabad 3, block 3","Aliyan","Dermotologist","5 years of experience",397862878,"scan","9AM-5PM");
+		    Location naz3("Nazimabad", "surgeries","Liaquat National","liaquat road","Faisal","cardiologist","9 years of experience",333024532,"xray","8AM-8PM");
+			Location naz3A("Nazimabad","surgeries", "Liaquat National", "liaquat road","Ashiq Hussain","Neuro surgeon","3 years of experience",323727672,"citiscan","9AM-5PM");
+			Location naz3B("Nazimabad", "surgeries","Liaquat National", "liaquat road","Areeba","physiotherapist","5 years of experience",219379483,"blood test","9AM-5PM");
 			cout<<"1: "; 
 			naz1.display();
 			cout<<endl;
@@ -322,11 +330,11 @@ int main()
 				break;
 			}
 		}
-		else if(loc=="Gulshan")
+		else if(loc=="Gulshan"||loc=="gulshan")
 		{
-			Location gul1("Gulshan", "Medicare", "gfgfgfgf","tayyab","general physician","you will be the first patient",00003,"medical","7PM-9PM");
-			Location gul2("Gulshan", "Agha Khan","adadadadad","ali","radiologist","high",2382839382,"mri","8AM-6PM");
-			Location gul3("Gulshan", "Liaquat National","ncc road","musawar","programmer", "normal",333022289,"xray","8AM-8PM");
+			Location gul1("Gulshan","Bones", "Medicare", "shaheedemillat rd","Tayyab","Rheumatologist","9 years of experience",33347479,"medical","7PM-9PM");
+			Location gul2("Gulshan", "Lungs","Agha Khan","near national stadium","Ali","Pulmonogistt","14 years of experience",213828393,"mri","8AM-6PM");
+			Location gul3("Gulshan","Psychiatry", "Liaquat National","ncc road","Musawar","Pediatrician", "1 year of experience",33304973,"xray","8AM-8PM");
 			cout<<"1: "; 
 			gul1.display();
 			cout<<endl;
@@ -352,17 +360,17 @@ int main()
 				break;
 			}
 		}
-		else if(loc=="Clifton")
+		else if(loc=="Clifton"||loc=="clifton")
 		{
-			Location clif1("Clifton", "Parklane", "70 clifton","muaaz","Psychic","2yrs",1111,"medical","2AM-7M");
-			Location clif1A("Clifton", "Parklane", "70 clifton","hahaha","Hakeem","3yrs",2222,"medical","2AM-7M");
-			Location clif1B("Clifton", "Parklane", "70 clifton","lol","dermo","3yrs",33333,"medical","2AM-7M");
-			Location clif2("Clifton", "Altamash","shahrah e ghalib road","nashit","oncologists","2yrs",2382856,"chemotherapy","8AM-6PM");
-			Location clif2A("Clifton", "Parklane", "70 clifton","Sahab","psych","6yrs",00004,"medical","2AM-7M");
-			Location clif2B("Clifton", "Parklane", "70 clifton","VIP","physio","5yrs",00004,"medical","2AM-7M");
-		   	Location clif3("Clifton", "South city","shahrah e firdousi","sameer","neurologists", "1yr",3289,"blood tests","8AM-8PM");
-			Location clif3A("Clifton", "Parklane", "70 clifton","Amitabh","FilmonWala","6yrs",00004,"medical","2AM-7M");
-			Location clif3B("Clifton", "Parklane", "70 clifton","grey","gyno","8yrs",00004,"medical","2AM-7M");
+			Location clif1("Clifton", "cancer", "Parklane", "70 clifton","Muaaz","Infectious disease specialist","8 years of experience",23768090,"medical","2AM-7M");
+			Location clif1A("Clifton", "Cancer", "Parklane", "70 clifton","Mahira","Oncologist","15 years of experience",333343244,"medical","2AM-7M");
+			Location clif1B("Clifton", "Cancer", "Parklane", "70 clifton","Anum","Endocrinologist","3 years of experience",21333442,"medical","2AM-7M");
+			Location clif2("Clifton", "Dental",  "Altamash","shahrah e ghalib road","Nashit","Oncologists","2 years of experience",23828562,"chemotherapy","8AM-6PM");
+			Location clif2A("Clifton", "Dental",  "Altamash", "shahrah e ghalib road","Sahab","Dentist","6 years of experience",33282893,"medical","2AM-7M");
+			Location clif2B("Clifton", "Dental",  "Altamash", "shahrah e ghalib road","Sameer","Otolaryngologist","7 years of experience",214869833,"medical","2AM-7M");
+		   	Location clif3("Clifton", "Brain disease", "South city","shahrah e firdousi","sameer","neurologists", "6 year of experience",345779892,"blood tests","8AM-8PM");
+			Location clif3A("Clifton", "Brain disease", "Parklane", "70 clifton","Amitabh","General practitioner","1 years of experience",334687981,"medical","2AM-7M");
+			Location clif3B("Clifton", "Brain disease",  "Parklane", "70 clifton","Haseeb","Radiologist","11 years of experience",221874948,"medical","2AM-7M");
 		   	cout<<"1: "; 
 		    clif1.display();
 		    cout<<endl;
@@ -409,20 +417,20 @@ int main()
 	{
 		cout<<"Enter Location: "<<endl;
 	    cin>>loc;
-		if(loc=="DHA")
+		if(loc=="DHA"||loc=="dha")
 	    {
-		    Location dha1("DHA", "NMC","adadadadad","taha","Attendant","bohat",1,"mri","8AM-6PM");
-		    Location dha2("DHA", "Bayview","bay","Kamran","Attendant","1 month",2,"xray","8AM-8PM");
-		    Location dha3("DHA", "NMC","adadadadad","anum","Senior nurse","11 years",3,"blood test","8AM-7PM");
-		    cout<<"1: Lab at Hospital: "; 
+		    Location dha1("DHA", "MRI", "NMC","adadadadad","taha","Attendant","bohat",1,"mri","8AM-6PM");
+		    Location dha2("DHA", "X-RAY", "Bayview","bay","Kamran","Attendant","1 month",2,"xray","8AM-8PM");
+		    Location dha3("DHA", "Blood Test", "NMC","adadadadad","anum","Senior nurse","11 years",3,"blood test","8AM-7PM");
+		    cout<<"1: Lab at Hospital:\n "; 
 		    dha1.display();
 		    
 		    cout<<endl;
-		    cout<<"2: Lab at Hospital:"; 
+		    cout<<"2: Lab at Hospital:\n"; 
 		    dha2.display();
 		   
 		    cout<<endl;
-		    cout<<"3: Lab at Hospital:"; 
+		    cout<<"3: Lab at Hospital:\n"; 
 		    dha3.display();
 		   
 		    cout<<endl;
@@ -442,19 +450,19 @@ int main()
 				break;		
 			}
 		}
-		else if(loc=="Nazimabad")
+		else if(loc=="Nazimabad"||loc=="nazimabad")
 		
 		{
-			Location naz1("Nazimabad", "Lifeline lab", "khjhkgjgkjh","Maqbool Hussain","Nurse","9 months",00002,"vaccination","9AM-5PM");
-			 Location naz2("Nazimabad", "Ziauddin lab","fvefvfdvdfvdfv","Ali Raza","Nurse","3 years",2,"ultra sound","8AM-8PM");
-		    Location naz3("Nazimabad", "Esaa lab","avfvfvf","Zainab khan","Nurse","9 years",3,"blood test","8AM-7PM");
-		    cout<<"1: Lab at Hospital: "; 
+			Location naz1("Nazimabad","Vaccination", "Lifeline lab", "khjhkgjgkjh","Maqbool Hussain","Nurse","9 months",00002,"vaccination","9AM-5PM");
+			 Location naz2("Nazimabad","Ultrasound",  "Ziauddin lab","fvefvfdvdfvdfv","Ali Raza","Nurse","3 years",2,"ultra sound","8AM-8PM");
+		    Location naz3("Nazimabad", "Blood test", "Esaa lab","avfvfvf","Zainab khan","Nurse","9 years",3,"blood test","8AM-7PM");
+		    cout<<"1: Lab at Hospital:\n"; 
 		    naz1.display();
 		    cout<<endl;
-		    cout<<"2: Lab at Hospital:"; 
+		    cout<<"2: Lab at Hospital:\n"; 
 		    naz2.display();
 		    cout<<endl;
-		    cout<<"3: Lab at Hospital:"; 
+		    cout<<"3: Lab at Hospital:\n"; 
 		    naz3.display();
 		    cout<<endl;
 		    cout<<"ENTER NUMBER TO CHOOSE HOSPITAL LAB"<<endl;
@@ -473,18 +481,18 @@ int main()
 				break;		
 			}
 		}
-		else if(loc=="Gulshan")
+		else if(loc=="Gulshan"||loc=="gulshan")
 		{
-			Location gul1("Gulshan", "Medicare", "gfgfgfgf","Alina khan","Attendant","5 years",00003,"vaccination","7PM-9PM");
-		    Location gul2("Gulshan", "Chughtai lab","fvefvfdvdfvdfv","Maryam Raza","Nure","6 year",2,"ultra sound","8AM-8PM");
-		    Location gul3("Gulshan", "Aga khan lab","avfvfvf","Fatima Aamir","Nurse","8 month",3,"blood test","8AM-7PM");
-		    cout<<"1: Lab at Hospital: "; 
+			Location gul1("Gulshan", "Vaccination", "Medicare", "gfgfgfgf","Alina khan","Attendant","5 years",00003,"vaccination","7PM-9PM");
+		    Location gul2("Gulshan", "Ultrasound", "Chughtai lab","fvefvfdvdfvdfv","Maryam Raza","Nure","6 year",2,"ultra sound","8AM-8PM");
+		    Location gul3("Gulshan", "Blood test", "Aga khan lab","avfvfvf","Fatima Aamir","Nurse","8 month",3,"blood test","8AM-7PM");
+		    cout<<"1: Lab at Hospital:\n"; 
 		    gul1.display();
 		    cout<<endl;
-		    cout<<"2: Lab at Hospital:"; 
+		    cout<<"2: Lab at Hospital:\n"; 
 		    gul2.display();
 		    cout<<endl;
-		    cout<<"3: Lab at Hospital:"; 
+		    cout<<"3: Lab at Hospital:\n"; 
 		    gul3.display();
 		    cout<<endl;
 		    cout<<"ENTER NUMBER TO CHOOSE HOSPITAL LAB"<<endl;
@@ -504,18 +512,18 @@ int main()
 			}
 			
 		}
-		else if(loc=="Clifton")
+		else if(loc=="Clifton"||loc=="clifton")
 		{
-			Location clif1("Clifton", "Dow lab", "rfrgergegegre","Ifrah Adil","nurse","3 years",00004,"Vaccination","2AM-7M");
-			Location clif2("Clifton", "Chughtai lab branch 2","fvefvfdvdfvdfv","Muqaddas Imran","Nurse","1 year",2,"ultra sound","8AM-8PM");
-		    Location clif3("Clifton", "DOW lab","avfvfvf","Wasim Ali","Attendant","2 years",3,"blood test","8AM-7PM");
-		    cout<<"1: Lab at Hospital: "; 
+			Location clif1("Clifton", "Vaccination", "Dow lab", "rfrgergegegre","Ifrah Adil","nurse","3 years",00004,"Vaccination","2AM-7M");
+			Location clif2("Clifton", "Ultra sound", "Chughtai lab branch 2","fvefvfdvdfvdfv","Muqaddas Imran","Nurse","1 year",2,"ultra sound","8AM-8PM");
+		    Location clif3("Clifton", "Blood test", "DOW lab","avfvfvf","Wasim Ali","Attendant","2 years",3,"blood test","8AM-7PM");
+		    cout<<"1: Lab at Hospital:\n"; 
 		    clif1.display();
 		    cout<<endl;
-		    cout<<"2: Lab at Hospital:"; 
+		    cout<<"2: Lab at Hospital:\n"; 
 		    clif2.display();
 		    cout<<endl;
-		    cout<<"3: Lab at Hospital:"; 
+		    cout<<"3: Lab at Hospital:\n"; 
 		    clif3.display();
 		    cout<<endl;
 		    cout<<"ENTER NUMBER TO CHOOSE HOSPITAL LAB"<<endl;
